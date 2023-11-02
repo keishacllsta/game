02d66683e99b56d4e0507a38afad5fc46f8bf82c
keishacllsta_ <keishacallista70@gmail.com>
2023-11-02 07:16:27 +0700

    game.py

--- game.py
+++ game.py
@@ -1,33 +1,33 @@
 import random
 import os
 
-player_score = 0
-com_score = 0
+skor_pemain = 0
+skor_komp = 0
 
-suit_obj = ["", "Gunting", "Batu", "kertas"]
-player_pick = None
-while player_pick != 0:
+pilihan = ["", "Gunting", "Batu", "kertas"]
+pilihan_pemain = None
+while pilihan_pemain != 0:
     os.system ("cls")
-    print(f"player [{player_score}]:[{com_score}] komputer")
+    print(f"player [{skor_pemain}]:[{skor_komp}] komputer")
     for i in range(1, 4):
-     print (f"{i}, {suit_obj[i]}")
+     print (f"{i}, {pilihan[i]}")
     print()
     print("0. keluar")
-    player_pick = int(input())
-    if player_pick != 0:
-        print(f"pilihan?? {suit_obj[player_pick]}")
+    pilihan_pemain = int(input())
+    if pilihan_pemain != 0:
+        print(f"pilihan?? {pilihan[pilihan_pemain]}")
 
-        com_pick = random.randint(1,3)
-        print(f"pilihan lawan{suit_obj[com_pick]}")
+        pilihan_komp = random.randint(1,3)
+        print(f"pilihan lawan{pilihan[pilihan_komp]}")
 
-        if player_pick == com_pick:
+        if pilihan_pemain == pilihan_komp:
             print("seri")
         else:
-            if (player_pick == 1 and com_pick == 2) or (player_pick == 2 and com_pick == 3) or (player_pick == 3 and com_pick == 1):
+            if (pilihan_pemain == 1 and pilihan_komp == 2) or (pilihan_pemain == 2 and pilihan_komp == 3) or (pilihan_pemain == 3 and pilihan_komp== 1):
                 print("win")
-                player_score +=1
+                skor_pemain +=1
             else:
                 print(" lose, try again")
-                com_score +=1
+                skor_komp +=1
 
 input ("enter to continue") 
